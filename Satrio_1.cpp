73606bcda3126921d6050843bbb589ce038997bf
satrio05 <113913970+satrio05@users.noreply.github.com>
2022-09-19 20:34:01 +0700

    Add files via upload

---
+++ Satrio_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+cout << "NAMA:GUNTUR"<<endl;
+cout << "KELAS:X RPL 2"<<endl;
+cout << "NILAI:85"<<endl;
+return 0;
+}
---
+++ Satrio_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+  
+  string nama;
+  string Umur;
+  cout << "Nama : SATRIO ARMANSYAH ";
+      cin >> nama;
+      
+      cout << "Umur : 15 ";
+      cin >> Umur;
+  return 0;
+}
